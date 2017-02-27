5e8b1b1cba6fa1bf8750e8b6c2d3826f6dce5e59
gisma <gisma@gis-ma.org>
2017-02-27 17:26:56 +0100

    fix indices

--- R/rs_rgbi.R
+++ R/rs_rgbi.R
@@ -1,35 +1,33 @@
-if (!isGeneric('rs_rgbi')) {
-  setGeneric('rs_rgbi', function(x, ...)
-    standardGeneric('rs_rgbi'))
-}
 #' RGB indices 
 #' 
 #' @description
-#' This function calculates various spectral indices from a RGB 
+#' This function calculates various spectral indices from a RGB. It returns at least red green and blue as splitted channels in a stack. Additionally you can choose RGB indices.
 #' \code{Raster*} object.  
 #' 
-#' @param rgb A \code{RasterStack} or \code{RasterBrick} object. 3
-#' bands are mandatory (usually red, green and blue).
+#' @param rgb a \code{RasterStack} or \code{RasterBrick} object. 3
+#' bands are mandatory (for RGB indices they should be: "red", "green" and "blue").
+#' @param rgbi the implemented RGB indices currently \link{seealso}
 
 #' @return 
+#' @seealso 
 #' A \code{RasterLayer} with the index calculates as:\cr
-#' VARI (green-red)/(green+red-blue). A Visible Atmospherically Resistant Index (VARI)\cr
 #' BI  sqrt((R**2+G**2+B*2)/3 Brightness Index\cr
-#' SI (R-B)/(R+B) Spectra Slope Saturation Index\cr
-#' HI (2*R-G-B)/(G-B) Primary colours Hue Index\cr
 #' CI (R-G)/(R+G) Soil Colour Index\cr
-#' RI R**2/(B*G**3) Redness Index\cr
+#' GLI (2*g - r - b)/(2*g + r + b) Green leaf index Vis Louhaichi et al. (2001)\cr
+#' HI (2*R-G-B)/(G-B) Primary colours Hue Index\cr
 #' NDTI (R-G)/(R+G) Normalized difference turbidity index Water\cr
 #' NGRDI (G-R)/(G+R) Normalized green red difference index (sometimes GRVI) Tucker (1979)
-#' VVI  (1-(r-30)/(r+30))*(1-(g-50)/(g+50))*(1-(b-1)/(b+1))\cr
+#' RI R**2/(B*G**3) Redness Index\cr
+#' SI (R-B)/(R+B) Spectral Slope Saturation Index\cr
 #' TGI  -0.5[190(R670-R550)-120(R670 - R480)] The triangular greenness index (TGI) estimates chlorophyll concentration in leaves and canopies\cr
-#' GLI Green leaf index Vis GLI (2*g - r - b)/(2*g + r + b) Louhaichi et al. (2001)
-
-
-
+#' VARI (green-red)/(green+red-blue). A Visible Atmospherically Resistant Index (VARI)\cr
+#' VVI  (1-(r-30)/(r+30))*(1-(g-50)/(g+50))*(1-(b-1)/(b+1))
 #' 
 #' @author
-#' Chris Reudenbach, (VVI) Florian Detsch, (VVI) Tim Appelhans
+#' Chris Reudenbach
+#' 
+#' @name rs_rgbIndices
+#' @export rs_rgbIndices
 #' 
 #' @references
 #' 
@@ -48,80 +46,118 @@ if (!isGeneric('rs_rgbi')) {
 #' Tucker, C.J., 1979. Red and photographic infrared linear combinations for monitoring vegetation. Remote Sensing of Environment 8, 127-150.
 #' 
 #' @seealso 
+#' For a comprehensive overview of remote sensing indices have a look at: \url{http://www.indexdatabase.de/db/i.php}(A database for remote sensing indices)\cr
 #' Wavelength ranges for overlapping digital camera bands are: red 580-670 nm, green 480-610 nm, and blue 400-520 nm (Hunt et al., 2005)
 #' http://digitalcommons.unl.edu/cgi/viewcontent.cgi?article=2161&context=usdaarsfacpub
 #' 
 #' @examples
+#' \notrun{
 #' library(raster)
 #' url <- "https://upload.wikimedia.org/wikipedia/commons/2/28/RGB_illumination.jpg"
 #' dFile <- download.file(url, "Image.jpg")
 #' img <- stack("Image.jpg") 
 #' plotRGB(img)
-#' rs_rgbi <- rs_rgbi(img)
-#' plot(rs_rgbi, col = gray(255:0/255))
+#' rgbi <- rgbI(img)
+#' plot(rgbI, col = gray(255:0/255))
+#' }
 #' 
-#' @export rs_rgbi
 #' 
-
-rs_rgbi<- function(rgb,rgbi=c("red","green","blue","VVI","VARI","NDTI","RI","CI","BI","SI","HI","TGI","GLI","NGRDI")) {
-  
-  ### prerequisites
+rs_rgbIndices<- function(red,green,blue,
+                      rgbi=c("VVI","VARI","NDTI","RI","CI","BI","SI","HI","TGI","GLI","NGRDI")) {
   
   ## compatibility check
-  if (nlayers(rgb) < 3)
+  if (raster::nlayers(rgb) < 3)
     stop("Argument 'rgb' needs to be a Raster* object with at least 3 layers (usually red, green and blue).")
   
   ### processing
   
   
   ## separate visible bands
-  red <- rgb[[1]]
-  green <- rgb[[2]]
-  blue <- rgb[[3]]
+  # red <- raster::raster(rgb[[1]])
+  # green <- raster::raster(rgb[[2]])
+  # blue <- raster::raster(rgb[[3]])
   
-  for (item in rgbi) {
-    ## calculate vvi
-    if (item=="VVI"){
+  indices <- lapply(rgbi, function(item){
+    ## calculate Visible Vegetation Index vvi
+    if (item == "VVI"){
+      cat("\ncalculate Visible Vegetation Index (VVI)")
       VVI <- (1 - abs((red - 30) / (red + 30))) * 
         (1 - abs((green - 50) / (green + 50))) * 
         (1 - abs((blue - 1) / (blue + 1)))
-    } else if (item=="VARI"){
-      # calculate VARI
-      VARI<-(green-red)/(green+red-blue)
-    } else if (item=="NDTI"){
+      names(VVI) <- "VVI"
+      return(VVI)
+      
+    } else if (item == "VARI") {
+      # calculate Visible Atmospherically Resistant Index (VARI)
+      cat("\ncalculate Visible Atmospherically Resistant Index (VARI)")
+      VARI <- (green - red) / (green + red - blue)
+      names(VARI) <- "VARI"
+      return(VARI)
+      
+    } else if (item == "NDTI") {
       ## Normalized difference turbidity index
-      NDTI<-(red-green)/(red+green)
-    } else if (item=="RI"){
+      cat("\ncalculate Normalized difference turbidity index (NDTI)")
+      NDTI <- (red - green) / (red + green)
+      names(NDTI) <- "NDTI"
+      return(NDTI)
+      
+    } else if (item == "RI") {
       # redness index
-      RI<-red**2/(blue*green**3)
-    } else if (item=="CI"){
+      cat("\ncalculate redness index (RI)")
+      RI <- red**2 / (blue*green**3)
+      names(RI) <- "RI"
+      return(RI)
+      
+    } else if (item == "CI") {
       # CI Soil Colour Index
-      CI <- (red-green)/(red+green)
-    } else if (item=="BI"){
+      cat("\ncalculate Soil Colour Index (CI)")
+      CI <- (red - green) / (red + green)
+      names(CI) <- "CI"
+      return(CI)
+      
+    } else if (item == "BI") {
       #  Brightness Index
-      BI<-sqrt((red**2+green**2+blue*2)/3)
-    } else if (item=="SI"){
+      cat("\ncalculate Brightness Index (BI)")
+      BI <- sqrt((red**2 + green**2 + blue*2) / 3)
+      names(BI) <- "BI"
+      return(BI)
+      
+    } else if (item == "SI") {
       # SI Spectra Slope Saturation Index
-      SI<-(red-blue)/(red+blue) 
+      cat("\ncalculate Spectra Slope Saturation Index (SI)")
+      SI <- (red - blue) / (red + blue) 
+      names(SI) <- "SI"
+      return(SI)
+      
     } else if (item=="HI"){    
       # HI Primary colours Hue Index
+      cat("\ncalculate Primary colours Hue Index (HI)")
       HI<-(2*red-green-blue)/(green-blue)
+      names(HI) <- "HI"
+      return(HI)
+      
     } else if (item=="TGI"){
       # Triangular greenness index
+      cat("\ncalculate Triangular greenness index (TGI)")
       TGI <- -0.5*(190*(red - green)- 120*(red - blue))
+      names(TGI) <- "TGI"
+      return(TGI)
+      
     } else if (item=="GLI"){
+      cat("\ncalculate Green leaf index (GLI)")
       # Green leaf index
       GLI<-(2*green-red-blue)/(2*green+red+blue)
+      names(GLI) <- "GLI"
+      return(GLI)
+      
     } else if (item=="NGRDI"){
-      # NGRDINormalized green red difference index 
+      # NGRDI Normalized green red difference index 
+      cat("\ncalculate Normalized green red difference index  (NGRDI)")
       NGRDI<-(green-red)/(green+red) 
+      names(NGRDI) <- "NGRDI"
+      return(NGRDI)
+      
     }  
-    
-  }
-  ## return rgbi
-  
-  result <- stack(red,green,blue,VVI,VARI,NDTI,RI,CI,BI,SI,HI,TGI,GLI,NGRDI)
-  result<-  stack(eval(result))
-  names(result) <- c("red","green","blue","VVI","VARI","NDTI","RI","CI","BI","SI","HI","TGI","GLI","NGRDI")
-  return(result)
+  })
+  return(raster::stack(indices))
 }
--- examples/usecase_simple_crownSegmentation.R
+++ examples/usecase_simple_crownSegmentation.R
@@ -72,43 +72,53 @@ if (!only_postprocessing) {
 
 if (crop) {
   cat("\n:: crop & adjust input data\n")
-  if (only_postprocessing) chmR <- raster::raster(paste0(path_output,"chm.tif"))
-  chmR <- raster::crop(chmR,ext)
-  rgb <- raster::stack(paste0(path_data,orthImg))
-  rgb <- raster::crop(rgb,ext)
-  rgb <- raster::resample(rgb, chmR, method = 'bilinear')
-  cat(":: calculate RGBI \n")
-  indices <- c("VVI","VARI") #names(rgbI)
-  rgbI <- uavRst::rs_rgbi(rgb)
-  
-  #converting them to SAGA
-  i <- 1
-  for (index in indices) {
-    uavRst:::R2SAGA(rgbI[[i]],index)
-    i <- i + 1
+  if (only_postprocessing) {
+    chmR <- raster::raster(paste0(path_output,"chm.tif"))
+    rgb <- raster::raster(paste0(path_output,"ortho.tif"))
+  } else {
+    chmR <- raster::crop(chmR,ext)
+    rgb <- raster::stack(paste0(path_data,orthImg))
+    rgb <- raster::crop(rgb,ext)
+    rgb <- raster::resample(rgb, chmR, method = 'bilinear')
+    raster::writeRaster(rgb,paste0(path_output,"ortho.tif"),
+                        overwrite = TRUE)
+    cat(":: calculate RGBI \n")
+    indices <- c("VVI","VARI") #names(rgbI)
+    rgbI <- uavRst::rs_rgbIndices(rgb[[1]],rgb[[2]],rgb[[3]],indices)
+    
+    #converting them to SAGA
+    i <- 1
+    for (index in indices) {
+      uavRst:::R2SAGA(rgbI[[i]],index)
+      i <- i + 1
+    }
   }  
 } else {
   cat("\n:: prepare and adjust ortho image\n")
-  if (only_postprocessing) chmR <- raster::raster(paste0(path_output,"chm.tif"))
-  rgb <- raster::stack(paste0(path_data,orthImg))
-  rgb <- raster::resample(rgb, chmR, method = 'bilinear')
-  cat(":: calculate RGBI \n")
-  rgbI <- uavRst::rs_rgbi(rgb)
-  #converting them to SAGA
-  indices <- c("VVI","VARI")
-  i <- 1
-  for (index in indices) {
-    uavRst:::R2SAGA(rgbI[[i]],index)
-    i <- i + 1
+  if (only_postprocessing) {
+    chmR <- raster::raster(paste0(path_output,"chm.tif"))
+    rgb <- raster::raster(paste0(path_output,"ortho.tif"))
+    }
+  else {
+    rgb <- raster::stack(paste0(path_data,orthImg))
+    rgb <- raster::resample(rgb, chmR, method = 'bilinear')
+    raster::writeRaster(rgb,paste0(path_output,"ortho.tif"),
+                        overwrite = TRUE)
+    cat(":: calculate RGBI \n")
+    #converting them to SAGA
+    indices <- c("VVI","VARI")
+    rgbI <- uavRst::rs_rgbIndices(rgb[[1]],rgb[[2]],rgb[[3]],indices)
+    i <- 1
+    for (index in indices) {
+      uavRst:::R2SAGA(rgbI[[i]],index)
+      i <- i + 1
+    }
   }
 }
-
+  
 #apply minium tree height to canopy height model (chm) -----------------------
 chmR[chmR < -minTreeHeight] <- minTreeHeight
 
-if (only_postprocessing) {
-  chmR <- raster::raster(paste0(path_output,"chmR.tif"))
-}
 
 # call tree crown segmentation 
 microbenchmark::microbenchmark(crowns <- fa_tree_segementation(chmR),times=1,minTreeAlt=minTreeHeight)
