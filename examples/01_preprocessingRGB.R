039a867cd70c26e5fee8d727040af0a4435fcd7d
gisma <gisma@gis-ma.org>
2018-01-15 10:13:19 +0100

    workflow classify

---
+++ examples/01_preprocessingRGB.R
@@ -0,0 +1,112 @@
+# pre-processing of RGB UAV ortho imagery
+# calculates RGB indices. statistics and haralick 
+#### packages
+rm(list =ls()
+require(link2GI)
+require(CAST)
+require(raster)
+require(foreach)
+require(doParallel)
+
+# switch for using  HaralickTextureExtraction 
+# for a review of a lot of feature extraction algorithms look at:
+# http://homepages.dcc.ufmg.br/~william/papers/paper_2012_JEI.pdf
+# NOTE IT TAKES A LOT OF TIME
+hara=TRUE
+# options are "all" "simple" "advanced"  "higher"
+haratype="advanced"
+# statistic: (mean,variance, curtosis, skewness)
+stat=TRUE
+#channels options "red" "green" "blue"
+channels<-c("green")
+# indices: options are ("VVI","VARI","NDTI","RI","CI","BI","SI","HI","TGI","GLI","NGRDI","GLAI")
+#c("VARI","NDTI","TGI","GLI","NGRDI","GLAI")
+indices <- c("VARI","NDTI","TGI","GLI","NGRDI","GLAI") 
+
+
+# kernelsize
+kernel<- 3
+
+if (hara) {
+  haratype=haratype
+} else {
+  haratype=""
+}
+
+
+# define project folder
+projRootDir <- "~/temp7/GRASS7"
+#predictDir <-"predict"
+# create project structure and export global pathes
+link2GI::initProj(projRootDir = projRootDir,
+                  projFolders = c("data/","output/","run/","fun","idx") )
+
+# set working directory
+setwd(path_run)
+
+# link GDAL and SAGA
+gdal <- link2GI::linkgdalUtils()
+otb<- link2GI::linkOTB()
+
+
+### ----- start preprocessing ---------------------------------------------------
+cat("\n::: preprocess input data...\n")
+
+# create list of image files to be processed
+imageFiles <- list.files(pattern="[.]tif$", path=paste0(path_data,"all"), full.names=TRUE)
+
+# stack the ortho images
+rgb<- lapply(imageFiles, FUN=raster::stack)
+
+
+### calculate indices and base stat export it to tif
+rgb_all<- flist<-list()
+
+# cl <- makeCluster(detectCores()-1)
+# registerDoParallel(cl)  
+# out <- foreach(i = 1:length(rgb),indices = indices) %dopar% {
+#   library(raster)  
+#   library(uavRst)
+  for (i in 1:length(rgb)){
+  rgb_rgbi<-raster::stack(rgb[[i]],uavRst::rgbIndices(rgb[[i]][[1]],rgb[[i]][[2]],rgb[[i]][[3]],indices))
+  
+  for (filterBand in channels){
+    if (filterBand=="red") bandNr <- 1
+    if (filterBand=="green") bandNr <- 2
+    if (filterBand=="blue") bandNr <- 3
+    # export single channel for synthetic band calculation
+    raster::writeRaster(rgb_rgbi[[bandNr]],paste0(filterBand,"_",basename(imageFiles[i])),overwrite=TRUE)
+    
+    if (stat){
+      uavRst:::otbLocalStat(fn = paste0(filterBand,"_",basename(imageFiles[i])),param=c(paste0(filterBand,"stat_",basename(imageFiles[i])),"4096", kernel))
+    }
+    
+    if (hara){
+      uavRst::otbTexturesHaralick(x = paste0(filterBand,"_",basename(imageFiles[i])),output_name=paste0(filterBand,"hara_",basename(imageFiles[i])),texture = haratype)
+    }
+    # delete single channel for synthetic channel calculation
+    # file.remove(paste0(filterBand,"_",basename(imageFiles[i])))
+    # get the rest in a list
+    file.remove(paste0(filterBand,"_",basename(imageFiles[i])))
+    flist<-append(flist, Sys.glob(paste0("*","_",basename(imageFiles[i]),"*")))
+    
+  }
+  # stack the results
+  rgb_all<-raster::stack(rgb_rgbi,raster::stack(flist))
+  if (raster::nlayers(rgb_all) > 256) stop(paste0("\n", raster::nlayers(rgb_all) ,"calculated...  Geotiffs may have 256... reduce your synthetic channels"))
+  
+  # export as geotiff
+  fn<-paste0(path_id,"/index_",basename(imageFiles[i]))
+  bnames<-uavRst:::makebNames(rgbi = indices, haratxt = haratype)
+  save(bnames,file = paste0(path_id,"/bnames_index_",basename(imageFiles[i]),".RData"))
+  names(rgb_all)<-bnames
+  raster::writeRaster(rgb_all,fn,overwrite=TRUE)
+  
+  # cleanup
+  file.remove(unlist(flist))
+  flist<-list()
+}
+
+#stopCluster(cl)
+cat("\n::: finished preprocessing RGB data...\n")
+
---
+++ examples/02_extractTrainData.R
@@ -0,0 +1,46 @@
+# extracting training data based on duigitized and classied geometries based on UAV ortho imagery
+rm(list =ls())
+require(link2GI)
+require(CAST)
+require(raster)
+require(foreach)
+require(doParallel)   
+   # folder containing shapefiles and images files for training purposes
+currentShptrainDir <- "training"
+# prefix for saved dataframe
+runname<-"test1"
+# classes numbers
+idNumber=c(1,2,3,4,5)
+# classes names
+idNames= c("green","nogreen","green","nogreen","nogreen")
+
+# project folder
+projRootDir <- "~/temp7/GRASS7"
+
+# create project structure and export pathes as global variables
+link2GI::initProj(projRootDir = projRootDir,
+                  projFolders = c("data/","output/","run/","fun","idx") )
+
+# set working directory
+setwd(path_run)
+
+# ----- start extraction ---------------------------------------------------
+# get image and geometry data for training purposes
+imageTrainFiles <- list.files(pattern="[.]tif$", path=paste0(path_data,currentShptrainDir), full.names=TRUE)
+geomTrainFiles  <- list.files(pattern="[.]shp$", path=paste0(path_data,currentShptrainDir), full.names=TRUE)
+rdataTrainFiles  <- list.files(pattern="[.]RData$", path=paste0(path_data,currentShptrainDir), full.names=TRUE)
+ imageTrainStack <- lapply(imageTrainFiles, FUN=raster::stack)
+ geomTrainStack  <- lapply(geomTrainFiles, FUN=raster::shapefile)
+
+# extract clean and format training data
+
+trainingDF <- uavRst::extractTrainData(rasterStack  = imageTrainStack,
+                                       trainPlots = geomTrainStack,
+                                       ids=idNumber,
+                                       idLabel= idNames,
+                                       trainDataFn = rdataTrainFiles
+                                       )
+
+cat("\n::: extraction...finsihed \n")
+
+
