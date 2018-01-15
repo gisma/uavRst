4a6bd8dce7b519252a5cc9980fd0fbf835fbd9ef
gisma <gisma@gis-ma.org>
2018-01-15 17:43:17 +0100

    fix

--- R/rs_classify.R
+++ R/rs_classify.R
@@ -146,7 +146,7 @@ extractTrainData<-function(rasterStack  = NULL,
   trainingDF$ID <- as.factor(trainingDF$ID)
   
   ## save dataframe
-  save(trainingDF, file = trainDataFn)
+  save(trainingDF, file = paste0(dirname(trainDataFn[[1]]),"traindat.RData"))
   return(trainingDF)
 }
 
