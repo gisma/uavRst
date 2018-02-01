5ff0694459a1a799259c85403dbb7ad2b6f551d0
gisma <gisma@gis-ma.org>
2018-02-01 18:30:55 +0100

    add usecase

--- inst/examples/useCaseTreeSegmentation.R
+++ inst/examples/useCaseTreeSegmentation.R
@@ -10,7 +10,7 @@
 #
 #        Whenever you run in trouble open an issue
 #        https://github.com/logmoc/msc-phygeo-class-of-2017-creuden/issues
-#        describe the problem with source code and add your sessionInfo() output
+#        describe the problem with source code and add your sessionInfo() plot(pl)output
 #
 #  Codebase: https://github.com/logmoc/msc-phygeo-class-of-2017-creuden
 # 
@@ -57,9 +57,6 @@ courseCode<-"gi"
 #--> current class session folder
 activeSessionFolder<-10
 
-#--> projection string 
-proj4="+proj=utm +zone=32 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
-
 #--> create complete projDir string
 projfolder<-file.path(rootDir,projDir,.Platform$file.sep,fsep = .Platform$file.sep)
 #--> make a list of all functions in the corresponding function folder and source these functions
@@ -70,21 +67,18 @@ makGlobalVar("path_run",gi_run)
 makGlobalVar("path_tmp",gi_run)
 makGlobalVar("path_output",gi_output)
 
-
+# delete run dir
 unlink(paste0(path_run,"*"), force = TRUE)
-# orthoimage filename
-rgb<-raster::stack("/home/creu/lehre/msc/active/msc-2017/data/gis/input/ref/geonode-_476000_5630000_1.tif")
+# load data
 load("/home/creu/lehre/msc/active/msc-2017/data/gis/output/0_5_10_15_20_50stat.RData")
-
 plot2<-raster::shapefile("/home/creu/lehre/msc/active/msc-2017/data/gis/input/ref/plot_UTM.shp")
 tree3<-raster::shapefile("/home/creu/lehre/msc/active/msc-2017/data/gis/input/ref/crowns.shp")
-
+# assign chm
 chm<-statLayer[[2]][[3]]
-
-
+# assign extent
 ext<- raster::extent(477393.,477460. ,5631938. , 5632003.)
 # link GDAL and SAGA
-# gdal <- link2GI::linkgdalUtils()
+gdal <- link2GI::linkgdalUtils()
 saga <- link2GI::linkSAGA()
 
 # ---------- start pr-processing ---------------------------------------------------  ---------------------------------------------------
@@ -93,48 +87,27 @@ cat("\n::: run pre processing...\n")
 chmR<-raster::crop(chm,ext)
 raster::writeRaster(chmR,paste0(gi_run,"chm.tif"),
                     overwrite = TRUE)
-#chmR<- chmR * -1 + raster::maxValue(chmR)
-indices<-c("HI", "GLI",  "GRVI")
+# index for segmentation default is chm
 indices <- c("chm")
-
- for (item in indices){
-   gdaldem(item,paste0(gi_run,"chm.tif"),paste0(gi_run,item,".tif"))
-   
-   gdalUtils::gdalwarp(paste0(path_run,item,".tif"), 
-                       paste0(path_run,item,".sdat"), 
-                       overwrite = TRUE,  
-                       of = 'SAGA',
-                       verbose = FALSE)
- }
+for (item in indices){
+  gdalUtils::gdalwarp(paste0(path_run,item,".tif"), 
+                      paste0(path_run,item,".sdat"), 
+                      overwrite = TRUE,  
+                      of = 'SAGA',
+                      verbose = FALSE)
+}
 
 
-# ----- start ADDON RGB image processing ---------------------------------------------------
-#  rgb <- raster::crop(rgb,ext)
-#  rgb <- raster::resample(rgb, chmR, method = 'bilinear')
-#  raster::writeRaster(rgb,paste0(path_output,"ortho.tif"),
-#                      overwrite = TRUE)
-# # #cat("::: calculate RGBI... \n")
-# rgbI <- uavRst::rgbIndices(rgb[[1]],rgb[[2]],rgb[[3]],indices)
-# 
-#  cat("\n convert indices to SAGA... \n")
-#  i <- 1
-#  for (index in indices) {
-#    cat("convert ",index,"\n")
-# #   gdalUtils::gdalwarp(paste0(path_run,index,".tif"),paste0(path_run,index,".sdat"), overwrite=TRUE,  of='SAGA') 
-#    uavRst::r2saga(rgbI[[i]],index)
-#    i <- i + 1
-#  }
-#
 # ----  start crown analysis --------------------------------------------------------
 
-# call tree crown segmentation 
+# call seeding process
 seeds <- uavRst::fa_treeSeeding(chmR,
-                                 minTreeAlt = 12.5,
-                                 crownMinArea = 3,
-                                 crownMaxArea = 225,
-                                 is0_join = 1, 
-                                 is0_thresh = 0.20, 
-                                 seeding = TRUE
+                                minTreeAlt = 12.5,
+                                crownMinArea = 3,
+                                crownMaxArea = 225,
+                                is0_join = 1, 
+                                is0_thresh = 0.20, 
+                                seeding = TRUE
 )
 
 # call tree crown segmentation 
@@ -144,11 +117,7 @@ crowns <- uavRst::fa_crown_segmentation(seeds = seeds,
                                         is3_sig2 = 0.05,
                                         is3_threshold = 0.00005,
                                         is3_seed_params = indices,
-                                        seeding = TRUE
 )
-
-# calculate potential insolation
-#uavRst::potInsolation("chm",pi_day = "01/06/2017",pi_day_stop = "30/06/2017",pi_hour_step = 1.0)
 
 cat("::: run post-classification...\n")
 # extract stats
@@ -171,16 +140,23 @@ trees_crowns <- uavRst::fa_basicTreeCrownFilter(crownFn = paste0(path_run,"crown
                                                 crownMinArea = 5,
                                                 crownMaxArea = 150,
                                                 mintreeAltParam = "chmQ20"
-                                                )
+)
 
+# vie it
+pl<-mapview(plot2)
+tc<-mapview(trees_crowns[[2]])
+tc+p
+
+# cut result is with reference
+finalTrees<-rgeos::gIntersection(plot2,trees_crowns[[2]],,byid = TRUE,)
+plot(finalTrees)
+
+
+# calculate potential insolation
+#uavRst::potInsolation("chm",pi_day = "01/06/2017",pi_day_stop = "30/06/2017",pi_hour_step = 1.0)
 
 # extract all pixvalues according to a vector geometry
 # pixvalues <- basicExtraction(x = chmR,fN = trees_crowns_2[[2]],responseCat = "ID")
 
-# cut result is with reference
-pl<-mapview(plot2)
-tc<-mapview(trees_crowns[[2]])
-tc+p
-finalTrees<-rgeos::gIntersection(plot2,trees_crowns[[2]],,byid = TRUE,)
-plot(finalTrees)
+
 cat(":: ...finsihed \n")
