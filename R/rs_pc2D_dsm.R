5a9b4094a2a6fb592ccaf5fec989a10b173b2322
gisma <gisma@gis-ma.org>
2018-03-10 19:33:19 +0100

    wrap up restructuring

---
+++ R/rs_pc2D_dsm.R
@@ -0,0 +1,135 @@
+
+#' Create a Digital Terrain Model from UAV generated point clouds by minimum altitude sampling
+#'
+#'@description
+#' Create a Digital Terrain Model from a high density point cloud as typically derived by an optical UAV retrieval. Due to the poor estimation of ground points 
+#' a minimum samopling approach is applied. It retrieves on a coarse sampling gridsize the minimum value and interpolates on these samples a surface grid with a higher target 
+#' resolution. this is a kind of an try and error process and provides fairly good results if the point cloud shows at least some real surface points on a not to coarse grid. 
+#'
+#'@author Chris Reudenbach
+#'
+#'@param laspcFile  character. default is \code{NULL} path  to the laz/las file(s)
+#'@param gisdbase_path character. default is \code{NULL} root directory of the project. NOTE the function creates two subfolder named \code{run} and \code{output}
+#'@param targetGridSize numeric. the resolution of the target DTM raster
+#'@param sampleMethod sampling method for point aggregation
+#'@param threshhold  numeric. percentile threshold
+#'@param proj4  character. valid proj4 string that should be assumingly the correct one
+#'@param giLinks list of link2GI cli pathes, default is NULL
+#'@param projFolder subfolders that will be created/linked for R related GRASS processing
+#'@param verbose to be quiet (1)
+#'@param cutExtent clip area
+#'
+#'@importFrom lidR tree_detection
+#'@importFrom lidR writeLAS
+#'@importFrom lidR readLAS
+#'@importFrom lidR lasclipRectangle
+#'@importFrom rlas readlasheader
+#'@export pc2D_dsm
+
+#'@examples
+#'\dontrun{
+#' pc2D_dsm(laspcFile =  "~/path/to/lasdata",
+#'        gisdbase_path = "~/temp5",
+#'        thin_with_grid = "0.5",
+#'        level_max = "5" ,
+#'        grid_size = "0.5")
+#'}
+
+pc2D_dsm <- function(laspcFile = NULL,
+                    gisdbase_path = NULL,
+                    sampleMethod = "max",
+                    threshold = 20 ,
+                    cutExtent = NULL,
+                    targetGridSize = 0.25,
+                    projFolder = NULL,
+                    proj4 = "+proj=utm +zone=32 +datum=WGS84 +units=m +no_defs",
+                    giLinks =NULL,
+                    verbose = FALSE) {
+  if (is.null(giLinks)){
+    giLinks <- get_gi()
+  }
+  gdal <- giLinks$gdal
+  saga <- giLinks$saga
+  otb <- giLinks$otb
+  sagaCmd<-saga$sagaCmd
+  path_OTB <- otb$pathOTB
+  
+  if (!verbose){
+    GV <- Sys.getenv("GRASS_VERBOSE")
+    Sys.setenv("GRASS_VERBOSE"=0)
+    ois <- get.ignore.stderrOption()
+    set.ignore.stderrOption(TRUE)}
+  
+  if (is.null(projFolder)) projFolder <-  c("data/","output/","run/","las/")
+  
+  # get/map the las binary folder and create the base command line
+  if (is.null(laspcFile)) stop("no directory containing las/laz files provided...\n")
+  else laspcFile <- path.expand(laspcFile)
+  name<-basename(laspcFile)
+  
+  # create project structure and export global paths
+  if (!nchar(Sys.getenv("GISDBASE")) > 0 ){
+  link2GI::initProj(projRootDir = tempdir() ,
+                    projFolders =  projFolder)
+  }
+
+
+  
+  if (!file.exists(paste0(path_run,name)))
+    cat(":: create copy of the las file at the working directory... \n")
+    file.copy(from = laspcFile,
+              to = paste0(path_run,name),
+              overwrite = TRUE)
+  cat(":: get extent of the point cloud \n")
+  if (!is.null(cutExtent)){
+    las<-lidR::lasclipRectangle(las, as.numeric(cutExtent[1]), as.numeric(cutExtent[3]), as.numeric(cutExtent[2]), as.numeric(cutExtent[4]))
+    lidR::writeLAS(las ,paste0(path_run,"cut_point_cloud.las"))
+    sp_param <- c(as.character(las$`Min X`),as.character(las$`Min Y`),as.character(las$`Max X`),as.character(las$`Max Y`))
+    # rename output file according to the extent
+    fn<- paste(sp_param ,collapse=" ")
+    tmp <- gsub(paste(sp_param ,collapse=" "),pattern = " ",replacement = "_")
+    name<-paste0(gsub(tmp,pattern = "[.]",replacement = "_"),".las")
+    file.rename(from =paste0(path_run,"cut_point_cloud.las"),
+                to = paste0(path_run,name))
+    
+  } else {
+    las<-rlas::readlasheader(paste0(path_run,name))
+    sp_param <- c(as.character(las$`Min X`),as.character(las$`Min Y`),as.character(las$`Max X`),as.character(las$`Max Y`))
+    # rename output file according to the extent
+    fn<- paste(sp_param ,collapse=" ")
+    tmp <- gsub(paste(sp_param ,collapse=" "),pattern = " ",replacement = "_")
+    name<-paste0(gsub(tmp,pattern = "[.]",replacement = "_"),".las")
+    file.rename(from =paste0(path_run,basename(laspcFile)),
+                to = paste0(path_run,name))
+  }
+  # copy it to the output folder
+  sp_param[5] <- proj4
+
+  link2GI::linkGRASS7(gisdbase = gisdbase_path,
+                      location = "pc2D_dsm", 
+                      spatial_params = sp_param, 
+                      resolution = sampleGridSize, 
+                      returnPaths = FALSE, 
+                      quiet = TRUE)
+  
+  cat(":: sampling ", sampleMethod, " altitudes using : ", sampleGridSize ,"meter grid size\n")  
+  ret <- rgrass7::execGRASS("r.in.lidar",
+                            flags  = c("overwrite","quiet","o","e","n"),
+                            input  = paste0(path_run,name),
+                            output = paste0("dsm",sampleGridSize),
+                            method = sampleMethod,
+                            resolution = sampleGridSize,
+                            intern = TRUE,
+                            ignore.stderr = TRUE
+  )
+  
+  
+  dsm<- raster::writeRaster(raster::raster(rgrass7::readRAST(paste0("dsm",sampleGridSize))),paste0(path_run,"dsm"), overwrite=TRUE,format="GTiff")
+
+  if (!verbose)  {
+    Sys.setenv("GRASS_VERBOSE"=GV)
+    set.ignore.stderrOption(ois)
+  }
+
+  return(dsm)
+}
